b96f5140be256f48a7222a13f46508c6eec09938
LazyManish <mbhattarai900@gmail.com>
2025-03-11 12:13:30 +0545

    added urls for register,login, logout

--- todo/api/urls.py
+++ todo/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import TodoCreateListView, TodoDetailView
+from .views import TodoCreateListView, TodoDetailView, UserLoginView, UserLogoutView, UserRegistrationViewset
 
 urlpatterns = [
+    path('register/', UserRegistrationViewset.as_view(), name='register'),
+    path('login/',UserLoginView.as_view(), name='login'),
+    path('logout/',UserLogoutView.as_view(), name='logout'),
     path('todos/', TodoCreateListView.as_view(), name='todo-list-create'),
     path('todos/<int:pk>/', TodoDetailView.as_view(), name='todo-detail'),
 ]
